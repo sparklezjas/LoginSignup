f24f0215616f00682f7b652e5cfcd9a80a465aad
Jasmine Erickson <sacredlightstudios@gmail.com>
2023-07-11 19:45:55 -0500

    Updated CSS for signup and login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useState } from 'react'
 import { useLogin } from '../hooks/useLogin'
+import background from "../imgs/cooking.jpg"
+
 
 const Login = () => {
     const [email, setEmail] = useState('')
@@ -13,26 +15,68 @@ const Login = () => {
     }
 
     return (
-        <form className='login' onSubmit={handleSubmit}>
-            <h3>Log in</h3>
+      <div style={{
+        backgroundImage: `url(${background})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}>
+        <form style={{
+            backgroundColor: "white",
+            border: "solid black 2px",
+            borderRadius: "10px", 
+            height: "40vh",
+            width: "40vh"
+        }} className='login'
+        onSubmit={handleSubmit}>
+            <h3 style={{
+            textAlign: "center", 
+            marginBottom:"10px",
+            marginTop: "20px",
+            padding: "10px", 
+            fontFamily: "Amatic SC",
+            fontWeight: "bold",
+            fontSize: "40px",
+            }}>Log in</h3>
 
-            <label>Email</label>
-            <input
+            <label style={{
+                margin: "15px"
+            }}>Email:</label>
+            <input style={{border: "solid black 2px", 
+            marginLeft: "28px"}}
                 type='email'
                 autoComplete='on'
                 onChange={(e) => setEmail(e.target.value)}
                 value={email}
                 />
-            <label>Password:</label>
-            <input
+            <label style={{
+                margin: "15px"
+            }}>Password:</label>
+            <input style={{border: "solid black 2px"}}
                 type='password'
                 autoComplete='on'
                 onChange={(e) => setPassword(e.target.value)}
                 value={password}
                 />
-                <button disabled={isLoading}>Log in</button>
+                <button
+                style={{margin: "10px",
+                textAlign: "center",
+                marginTop: "30px",
+                marginLeft: "105px",
+                fontSize: "18px",
+                borderRadius: "5px",
+                padding: "5px 20px",
+                backgroundColor: "#ffebdd"}}
+                disabled={isLoading}>Log in</button>
                 {error && <div className='error'>{error}</div>}
         </form>
+      </div>
 )
 }
 

7274cb1f942a32ebe6dd8180b375b9b6da6a576b
Jasmine Erickson <sacredlightstudios@gmail.com>
2023-07-12 09:43:27 -0500

    Added protected routes on app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import Login from './components/Login';
 import Signup from './components/Signup';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -8,24 +8,26 @@ import Home from './components/Home';
 import ShowAllRecipes from './components/ShowAllRecipes';
 import NewRecipe from './components/NewRecipe';
 import OneRecipe from './components/OneRecipe';
-
+import { useAuthContext } from './hooks/useAuthContext';
 import EditRecipe from './components/EditRecipe';
 import SearchResults from './components/SearchResults';
 
 
 function App() {
+  const { user } = useAuthContext()
+
   return (
     <div className="App">
       <BrowserRouter>
       <Navigation />
         <Routes>
-          <Route path="/login" element = {<Login/>}/>
-          <Route path="/signup" element = {<Signup/>}/>
-          <Route path="/" element={<Home />} />
-          <Route path="/all/recipes" element={<ShowAllRecipes />} />
-          <Route path="/new/recipe" element={<NewRecipe />} />
-          <Route path="/one/recipe/:id" element={<OneRecipe />} />
-          <Route path="/update/recipe/:id" element={<EditRecipe />} />
+          <Route path="/login" element= {!user ? <Login/>: <Navigate to="/"/>}/>
+          <Route path="/signup" element= {!user ? <Signup/>: <Navigate to="/"/>}/>
+          <Route path="/" element={user ? <Home /> : <Navigate to="/login" />}/>
+          <Route path="/all/recipes" element={user ? <ShowAllRecipes /> : <Navigate to="/login" />}/>
+          <Route path="/new/recipe" element={user ? <NewRecipe /> : <Navigate to="/login" />}/>
+          <Route path="/one/recipe/:id" element={user ? <OneRecipe /> : <Navigate to="/login" />}/>
+          <Route path="/update/recipe/:id" element={user? <EditRecipe /> : <Navigate to="/login" />}/>
         </Routes>
       </BrowserRouter>
     </div>
